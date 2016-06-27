2095a9d55f7e5a52655ad4ed2fda7b86003775f7
Takenobu_Endo <takenobu_endo@jmas.co.jp>
2016-06-27 14:18:19 +0900

    Initial Commit

---
+++ BeacappiOSSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeacappiOSSample
+//
+//  Created by 遠藤武宣 on 2016/06/27.
+//  Copyright © 2016年 遠藤武宣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeacappiOSSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeacappiOSSample
+//
+//  Created by 遠藤武宣 on 2016/06/27.
+//  Copyright © 2016年 遠藤武宣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
